5781e6d271d5055402c6c6c66bc5abf32bcc01c1
Justin Walrath <walrathjaw@gmail.com>
2014-10-27 19:50:55 -0500

    formatted a basic insert page.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,15 +2,29 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 import dao.PasteDao
 import org.bson.types.ObjectId
 import models.PasteTO
 
 object Application extends Controller {
 
+  val newPasteForm = Form(
+	tuple(
+	    "title" -> text,
+	    "content" -> text
+	)
+  )
+  
   def index = Action {
-	  val result = PasteDao.createPaste(new ObjectId("54485f901adee7b53870bacb"), "sample", "Sample Message", false)
-	  Ok(views.html.index(result.getAs[String]("pasteId").get))
+	  Ok(views.html.index("Create A New Paste"))
+  }
+  
+  def createPaste = Action { implicit request =>
+	val (title, content) = newPasteForm.bindFromRequest.get
+	val result = PasteDao.createPaste(new ObjectId("54485f901adee7b53870bacb"), title, content, false)
+	Ok(views.html.index(result.getAs[String]("pasteId").get))
   }
   
   def displayPaste(pasteId: String) = Action {

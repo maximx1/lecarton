9745ba77d3ce4ef5592d4d77654af9e18705dda7
Justin Walrath <walrathjaw@gmail.com>
2014-11-08 04:00:41 -0600

    Added business logic for login and checking if profile exists. references #2

--- app/business/ProfileManager.scala
+++ app/business/ProfileManager.scala
@@ -3,6 +3,7 @@ package business
 import dao.ProfileDao
 import models.ProfileTO
 import org.bson.types.ObjectId
+import org.mindrot.jbcrypt.BCrypt
 
 /**
  * Business logic for handling profiles.
@@ -25,5 +26,34 @@ class ProfileManager {
    */
   def userExists(userId: ObjectId): Boolean = profileDao.queryUserProfileById(ProfileTO(userId, null, null, null)) != null
 
+  /**
+   * Creates a new userafter checking that one doesn't already exist.
+   * @param username The username to create with.
+   * @param password The password to create with.
+   * @param email The email to create with.
+   * @return New ProfileTO if successful or null ortherwise.
+   */
+  def createUser(username: String, password: String, email: String): ProfileTO = {
+    if(userExists(username)) {
+      return null
+    }
+    else {
+      val newObject = profileDao.createUserProfile(username, password, email)
+      return ProfileTO(newObject.getAs[ObjectId]("_id").get, newObject.getAs[String]("username").get, null, newObject.getAs[String]("email").get)
+    }
+  }
 
+  /**
+   * Attempts to log in a profile.
+   * @param username The username to log in with.
+   * @param password The password to log in with.
+   * @return The ProfileTO of the logged in user profile.
+   */
+  def attemptLogin(username: String, password: String): ProfileTO = {
+    val result = profileDao.queryUserProfileByUsername(ProfileTO(null, username, null, null))
+    if(result != null && BCrypt.checkpw(password, result.password)) {
+      return result
+    }
+    return null
+  }
 }
--- test/business/ProfileManagerTest.scala
+++ test/business/ProfileManagerTest.scala
@@ -1,8 +1,11 @@
 package business
 
+import com.mongodb.casbah.Imports._
+import com.mongodb.casbah.commons.MongoDBObject
 import dao.ProfileDao
 import models.ProfileTO
 import org.bson.types.ObjectId
+import org.mindrot.jbcrypt.BCrypt
 import org.scalatest.{FlatSpec, Matchers, BeforeAndAfter}
 import org.scalatest.mock.MockitoSugar
 import org.mockito.Mockito
@@ -24,27 +27,84 @@ class ProfileManagerTest extends FlatSpec with Matchers with MockitoSugar with B
     val query = ProfileTO(null, "AAAA", null, null)
     val expectedResponse = ProfileTO(new ObjectId, "AAAA", "asdf", "sample@email.com")
     Mockito.when(profileManager.profileDao.queryUserProfileByUsername(query)).thenReturn(expectedResponse)
-    profileManager.userExists("AAAA") should be (true)
+    profileManager.userExists("AAAA") should be(true)
   }
 
   it should "return false if username is not found" in {
     val query = ProfileTO(null, "AAAA", null, null)
     val expectedResponse = null
     Mockito.when(profileManager.profileDao.queryUserProfileByUsername(query)).thenReturn(expectedResponse)
-    profileManager.userExists("AAAA") should be (false)
+    profileManager.userExists("AAAA") should be(false)
   }
 
   it should "return true if user id is found" in {
     val query = ProfileTO(new ObjectId, null, null, null)
     val expectedResponse = ProfileTO(query._id, "AAAA", "asdf", "sample@email.com")
     Mockito.when(profileManager.profileDao.queryUserProfileById(query)).thenReturn(expectedResponse)
-    profileManager.userExists(query._id) should be (true)
+    profileManager.userExists(query._id) should be(true)
   }
 
   it should "return false if user id is not found" in {
     val query = ProfileTO(new ObjectId, null, null, null)
     val expectedResponse = null
     Mockito.when(profileManager.profileDao.queryUserProfileById(query)).thenReturn(expectedResponse)
-    profileManager.userExists(query._id) should be (false)
+    profileManager.userExists(query._id) should be(false)
   }
+
+  "Create user profile" should "be able to create a new user profile" in {
+    val actual = successfulCreateUserBase
+    actual.username should be ("tom")
+    actual.email should be ("sample@email.com")
+  }
+  
+  it should "block creation if user already exists" in {
+    val query = ProfileTO(null, "tom", null, null)
+    val expectedResponse = ProfileTO(new ObjectId, "tom", "1234", "sample@email.com")
+    Mockito.when(profileManager.profileDao.queryUserProfileByUsername(query)).thenReturn(expectedResponse)
+    profileManager.createUser("tom", "1234", "sample@email.com") should be (null)
+  }
+  
+  it should "strip the password from the returning object" in {
+    val actual = successfulCreateUserBase
+    actual.password should be (null)
+  }
+
+  "A login" should "be able to occur successfully if the moons are aligned" in {
+    val query = ProfileTO(null, "tom", null, null)
+    val expectedResponse = ProfileTO(new ObjectId, "tom", BCrypt.hashpw("1234", BCrypt.gensalt(4)), "sample@email.com")
+    Mockito.when(profileManager.profileDao.queryUserProfileByUsername(query)).thenReturn(expectedResponse)
+    val actual = profileManager.attemptLogin("tom", "1234")
+    actual should not be null
+  }
+
+  it should "fail if the user lookup result is null" in {
+    val query = ProfileTO(null, "tom", null, null)
+    val expectedResponse = null
+    Mockito.when(profileManager.profileDao.queryUserProfileByUsername(query)).thenReturn(expectedResponse)
+    val actual = profileManager.attemptLogin("tom", "1234")
+    actual should be (null)
+  }
+
+  it should "fail if the passwords do not match" in {
+    val query = ProfileTO(null, "tom", null, null)
+    val expectedResponse = ProfileTO(new ObjectId, "tom", BCrypt.hashpw("1234", BCrypt.gensalt(4)), "sample@email.com")
+    Mockito.when(profileManager.profileDao.queryUserProfileByUsername(query)).thenReturn(expectedResponse)
+    val actual = profileManager.attemptLogin("tom", "1235")
+    actual should be (null)
+  }
+  
+  lazy val successfulCreateUserBase = {
+    val query = ProfileTO(null, "tom", null, null)
+    val expectedQueryUsernameResponse = null
+    Mockito.when(profileManager.profileDao.queryUserProfileByUsername(query)).thenReturn(expectedQueryUsernameResponse)
+    Mockito.when(profileManager.profileDao.createUserProfile("tom", "1234", "sample@email.com")).thenReturn(successfullyReturnedDocument)
+    profileManager.createUser("tom", "1234", "sample@email.com")
+  }
+  
+  lazy val successfullyReturnedDocument: MongoDBObject = MongoDBObject(
+    "_id" -> new ObjectId(),
+    "username" -> "tom",
+    "password" -> "1234",
+    "email" -> "sample@email.com"
+  )
 }

bc055c91c35761da6b652df0acbd4f2b7fc8f4fc
Justin Walrath <walrathjaw@gmail.com>
2014-11-09 03:02:17 -0600

    Added login and logout. closes #27 closes #10 closes #9 closes #8

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,12 @@
 package controllers
 
-import business.PasteManager
+import business.{ProfileManager, PasteManager}
 import com.sun.xml.internal.bind.v2.TODO
 import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
-import dao.PasteDao
+import dao.{AnonUserManager, PasteDao}
 import org.bson.types.ObjectId
 import models.PasteTO
 
@@ -18,29 +18,87 @@ object Application extends Controller {
         "content" -> text
     )
   )
+
+  val loginForm = Form(
+    tuple(
+      "username" -> text,
+      "password" -> text
+    )
+  )
+
+  val createProfileForm = Form(
+    tuple(
+      "username" -> text,
+      "password1" -> text,
+      "password2" -> text,
+      "email" -> text
+    )
+  )
   
-  def index = Action {
-	  Ok(views.html.index("Create A New Paste"))
+  def index = Action { implicit request =>
+	  Ok(views.html.index("Create A New Paste")(request.session))
   }
   
   def createPaste = Action { implicit request =>
     val (title, content) = newPasteForm.bindFromRequest.get
-    val result = (new PasteDao).createPaste(new ObjectId("54485f901adee7b53870bacb"), title, content, false)
-    Ok(views.html.index(result.getAs[String]("pasteId").get))
+    val sessionUserId = request.session.get("loggedInUser_id")
+    val owner: ObjectId = if (sessionUserId.isEmpty) AnonUserManager.anonId.value else new ObjectId(sessionUserId.get)
+    val result = (new PasteDao).createPaste(owner, title, content, false)
+    Ok(views.html.index(result.getAs[String]("pasteId").get)(request.session))
   }
   
-  def displayPaste(pasteId: String) = Action {
+  def displayPaste(pasteId: String) = Action { implicit request =>
 	  val pasteQuery = PasteTO(null, pasteId, null, null, null, false)
 	  val result = (new PasteDao).queryPasteByPasteId(pasteQuery)
-	  Ok(views.html.paste(result))
+	  Ok(views.html.paste(result)(request.session))
   }
 
-  def search(searchScope: String, searchString: String) = Action {
+  def search(searchScope: String, searchString: String) = Action { implicit request =>
     val result = (new PasteManager).handlePasteSearch(searchScope, searchString)
-    Ok(views.html.searchResults(result, searchScope, searchString))
+    Ok(views.html.searchResults(result, searchScope, searchString)(request.session))
   }
 
-  def login = TODO
-  def attemptLogin(username: String, password: String) = TODO
+  def login = Action { implicit request =>
+    Ok(views.html.login(null)(request.session))
+  }
 
+  def attemptLogin = Action { implicit request =>
+    val (username, password) = loginForm.bindFromRequest.get
+    val result = (new ProfileManager).attemptLogin(username, password)
+    if(result == null) {
+      Ok(views.html.login("Username/password not found")(request.session))
+    }
+    else {
+      Redirect(routes.Application.index) withSession("loggedInUsername" -> result.username, "loggedInUser_id" -> result._id.toString)
+    }
+  }
+
+  def logout = Action { implicit request =>
+    Redirect(routes.Application.login) withNewSession
+  }
+
+  def createProfile = Action { implicit request =>
+    Ok(views.html.createProfile(null)(request.session))
+  }
+
+  def attemptCreateProfile = Action { implicit request =>
+    val (username, password1, password2, email) = createProfileForm.bindFromRequest.get
+    val profileManager: ProfileManager = new ProfileManager()
+
+    if(profileManager.userExists(username)) {
+      Ok(views.html.createProfile("Username is already taken")(request.session))
+    }
+    else if(password1 != password2) {
+      Ok(views.html.createProfile("Both passwords were not equal")(request.session))
+    }
+    else {
+      val result = profileManager.createUser(username, password1, email)
+      if(result == null) {
+        Ok(views.html.createProfile("There was an unspecified error, you should try to log in again")(request.session))
+      }
+      else {
+        Ok(views.html.index("Account successfully created, ready to create new paste")(request.session))
+      }
+    }
+  }
 }
